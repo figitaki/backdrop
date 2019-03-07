88e9d7631b7a552b653adb3c37ebeb6ca4819d51
Carey Janecka <carey.janecka@coinbase.com>
2019-03-06 20:29:01 -0800

    Updates

---
+++ precache-manifest.483e4e80e3ccc4006b14c397a27a618c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86486aff4ae3239a65d2",
+    "url": "/backdrop/static/css/main.dff1f97f.chunk.css"
+  },
+  {
+    "revision": "86486aff4ae3239a65d2",
+    "url": "/backdrop/static/js/main.86486aff.chunk.js"
+  },
+  {
+    "revision": "9f3bb0ed77ec39bcda44",
+    "url": "/backdrop/static/js/runtime~main.9f3bb0ed.js"
+  },
+  {
+    "revision": "d17a08a7d1225745bc3b",
+    "url": "/backdrop/static/js/2.d17a08a7.chunk.js"
+  },
+  {
+    "revision": "1fba0f4bcd1e1773e725691840a2cd87",
+    "url": "/backdrop/static/media/background.1fba0f4b.svg"
+  },
+  {
+    "revision": "d02c4e833f06ca5d7b0e95712a316de7",
+    "url": "/backdrop/index.html"
+  }
+];
